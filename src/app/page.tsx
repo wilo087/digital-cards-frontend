2f9d0e90cae95544b84e755dad0cb63febbd3c03
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-04-04 04:14:20 -0400

    Setting up ts-standard linter and adding new page with parameter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home (): React.ReactElement {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -14,16 +15,16 @@ export default function Home() {
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
-          <Link href={`profile`}>Go to profile</Link>
+          <Link href='profile'>Go to profile</Link>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             By{' '}
             <Image
-              src="/images/vercel.svg"
-              alt="Vercel Logo"
+              src='/images/vercel.svg'
+              alt='Vercel Logo'
               className={styles.vercelLogo}
               width={100}
               height={24}
@@ -36,23 +37,23 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/images/next.svg"
-          alt="Next.js Logo"
+          src='/images/next.svg'
+          alt='Next.js Logo'
           width={180}
           height={37}
           priority
         />
         <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          <Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
         </div>
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2 className={inter.className}>
             Docs <span>-&gt;</span>
@@ -63,10 +64,10 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2 className={inter.className}>
             Templates <span>-&gt;</span>
@@ -75,10 +76,10 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2 className={inter.className}>
             Deploy <span>-&gt;</span>
--- src/app/profile/[uuid]/page.tsx
+++ src/app/profile/[uuid]/page.tsx
@@ -1,9 +1,10 @@
-import { Inter } from 'next/font/google'
+import React from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
 
-const inter = Inter({ subsets: ['latin'] })
+// import { Inter } from 'next/font/google'
+// const inter = Inter({ subsets: ['latin'] })
 
 interface params extends Params {
   uuid?: string[] | number[]
@@ -11,16 +12,16 @@ interface params extends Params {
 
 export const metadata = {
   title: 'Card Data'
-};
+}
 
-export default function Profile({ params }: params) {
-  const { uuid } = params;
+export default function Profile ({ params }: params): React.ReactElement {
+  const { uuid } = params
 
   return (
     <main className={styles.main}>
       Profile page
 
-      <Link href={`/`}>Go back {uuid}</Link>
+      <Link href='/'>Go back {uuid}</Link>
     </main>
   )
 }
