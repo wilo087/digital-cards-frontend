8a3c761d9c0b2b1901a805bc994b55bf5ac9d4cc
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-04-07 22:33:21 -0400

    Adding Material UI and login page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,10 @@ interface Props {
 export default function RootLayout ({ children }: Props): React.ReactElement {
   return (
     <html lang='en'>
+      <head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </head>
+
       <body>{children}</body>
     </html>
   )
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import UserButton from './components/UserButton'
+import React from 'react'
+
+export default function Login (): React.ReactElement {
+  return (
+    <main>
+      <h1>This is the login page 2</h1>
+      <UserButton />
+    </main>
+  )
+}
